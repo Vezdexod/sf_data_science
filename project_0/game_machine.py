33b410ff602a73c072216060bbdfd6c6abf5bd33
Vezdexod <vezdexod@internet.ru>
2021-10-09 22:38:50 +0800

    itog 09.10.21

--- project_0/game_machine.py
+++ project_0/game_machine.py
@@ -1,9 +1,10 @@
 """Игра угадай число
 Компьютер сам загадывает и сам угадывает число
 """
+
 import numpy as np
 
-predict_number_ls = [] # Создание списка с рандомными значениями каждой из 1000 итерациЙ
+predict_number_ls = []
 
 def random_predict(number: int = 50) -> int:
     """Рандомно задаем число и угадываем его по заданному алгоритму
@@ -15,10 +16,10 @@ def random_predict(number: int = 50) -> int:
         int: Число попыток
     """
     count = 0                   # Обнуляем число попыток
-    global predict_number_ls    # Объявляем глобальный список
+    global predict_number_ls    # Объявляем глобальный список рандомных чисел
     
     predict_number = np.random.randint(1, 101)  # Генерируем "загаданное" число
-    predict_number_ls.append(predict_number)
+    predict_number_ls.append(predict_number)    # Добавляем заданное число в список
     # Основа задания
     while True:
         count += 1
@@ -55,18 +56,14 @@ def score_game(random_predict) -> int:
         int: среднее количество попыток
     """
     count_ls = []
-    dict_of_numbers = {}
     i = 0
     while i < 1000:#random_array:        
-        count_ls.append(random_predict())
-        dict_of_numbers.update({predict_number_ls[i]:count_ls[i]})
+        count_ls.append(random_predict())        
         i += 1  
-    
-    print(f"Соотношение загаданного значения и числа попыток отгадать его: {dict_of_numbers}")
-    score = int(np.mean(count_ls)) #Среднее число попыток отгадать загаданное значение
-    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+        
+    score = int(np.mean(count_ls)) 
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток.")
     return score
-
 
 if __name__ == "__main__":
     # RUN
