f5eadbab4d7cab0d789fa3abcef34c7059ed1346
Vezdexod <vezdexod@internet.ru>
2021-11-08 01:29:28 +0800

    07.11.21

--- Data_Science_memory/test.py
+++ Data_Science_memory/test.py
@@ -2,40 +2,24 @@ import pandas as pd
 from pandas.core.tools.datetimes import to_datetime
 import re 
 
-def get_year_release(arg):
-    #находим все слова по шаблону "(DDDD)"
-    candidates = re.findall(r'\(\d{4}\)', arg) 
-    # проверяем число вхождений
-    if len(candidates) > 0:
-        #если число вхождений больше 0,
-	#очищаем строку от знаков "(" и ")"
-        year = candidates[0].replace('(', '')
-        year = year.replace(')', '')
-        return int(year)
-    else:
-        #если год не указан, возвращаем None
-        return None
+covid_data = pd.read_csv('C:/Python/PYTHON-13/covid_data.csv', sep=',')
+vac_data = pd.read_csv('C:/Python/PYTHON-13/country_vaccinations.csv', sep=',')
+covid_df = covid_data.copy()
+vac_df = vac_data.copy()
+vac_df = vac_df[
+    ['country', 'date', 'total_vaccinations', 
+     'people_vaccinated', 'people_vaccinated_per_hundred',
+     'people_fully_vaccinated', 'people_fully_vaccinated_per_hundred',
+     'daily_vaccinations', 'vaccines']
+]
+print(covid_df.head())
+print(vac_df.head())
 
 
-rating_movies_data = pd.read_csv('C:/Python/ratings_movies/ratings_movies.csv', sep=',')
 
-ratings_df = rating_movies_data.copy()
-#print(ratings_df)
 
-#12.8.1
-ratings_df['year_release'] = ratings_df['title'].apply(get_year_release)
-#print(ratings_df.info())
 
-#12.8.2
-for_1999 = ratings_df[(ratings_df['year_release'] == 1999)]
-ratings_1999 = for_1999.groupby(['title'])['rating'].mean()
-#print(ratings_1999.sort_values())
 
-#12.8.3
-for_2010 = ratings_df[(ratings_df['year_release'] == 2010)]
-ratings_2010 = for_2010.groupby(['genres'])['rating'].mean()
-#print(ratings_2010.sort_values())
 
-#12.8.4
-user_fan = ratings_df.groupby('userId')['genres'].nunique().sort_values()
-#print(user_fan)
+
+
--- Data_Science_memory/test2.py
+++ Data_Science_memory/test2.py
@@ -1,14 +1,3 @@
-import pandas as pd
-
-a = pd.DataFrame({'A': ['a', 'b', 'c'], 'B': [103, 214, 124], 'C': [1, 4, 2]})
-b = pd.DataFrame({'V': ['d', 'b', 'c'], 'U': [1393.7, 9382.2, 1904.5], 'C': [1, 3, 2]})
-
-
-print(a)
-print(b)
-print(a.merge(b, how='right', on='C'))
-
-
-
-
-
+import plotly
+import plotly.express as px
+print(plotly.__version__)
