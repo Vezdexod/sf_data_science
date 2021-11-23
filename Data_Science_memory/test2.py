cdb2337a6118471e5cc8e5f5062111b059543c81
Vezdexod <vezdexod@internet.ru>
2021-11-24 00:13:09 +0800

    23.11.21

--- Data_Science_memory/test2.py
+++ Data_Science_memory/test2.py
@@ -1,28 +1,14 @@
-class User():
-    def __init__(self, email, password, balance):
-        self.email = email
-        self.password = password
-        self.balance = balance
-        self.dict = {email:password,}
-        
-    def login(self, email, password):
-        if self.dict[email] == password:
-            #print(self.dict[email], 'and', password)
-            return True
-        else:
-            #print(self.dict[email], 'and', password)
-            return False
-    def update_balance(self, count):        
-        self.balance += count
-        return self.balance
-            
-            
-user = User("gosha@roskino.org", "qwerty", 20_000)
-user.login("gosha@roskino.org", "qwerty123")
-# => False
-user.login("gosha@roskino.org", "qwerty")
-# => True
-user.update_balance(200)
-user.update_balance(-500)
-print(user.balance)
-# => 19700
+def get_experience(arg):
+    if arg is np.nan:
+        return None
+    year_words=['год', 'года', 'лет']
+    month_words=['месяц', 'месяца', 'месяцев']
+    arg_splitted = arg.split(' ')[:7]
+    years = 0
+    months = 0
+    for index, item in enumerate (arg_splitted):
+        if item in year_words:
+            years = int(arg_splitted[index-1])
+        if item in month_words:
+            months = int(arg_splitted[index-1])
+    return int(years*12 + months)
