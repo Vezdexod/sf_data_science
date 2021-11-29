1e6f5ca954959cfd0cd84b33fbf525e6b30bb4bf
Vezdexod <vezdexod@internet.ru>
2021-11-30 00:25:48 +0800

    29.11.21

--- Data_Science_memory/test2.py
+++ Data_Science_memory/test2.py
@@ -1,14 +1,4 @@
-def get_experience(arg):
-    if arg is np.nan:
-        return None
-    year_words=['год', 'года', 'лет']
-    month_words=['месяц', 'месяца', 'месяцев']
-    arg_splitted = arg.split(' ')[:7]
-    years = 0
-    months = 0
-    for index, item in enumerate (arg_splitted):
-        if item in year_words:
-            years = int(arg_splitted[index-1])
-        if item in month_words:
-            months = int(arg_splitted[index-1])
-    return int(years*12 + months)
+if 'abc' in 'aaa asc abc':
+    print('aaa')
+else: 
+    print('bbb')
