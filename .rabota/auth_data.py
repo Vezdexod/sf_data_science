6cf6da73348b7c4ebcd0dd2cbbb30276b7601690
Vezdexod <vezdexod@internet.ru>
2023-01-14 22:20:52 +0800

    for быстрее apply

---
+++ .rabota/auth_data.py
@@ -0,0 +1,5 @@
+# токен с привязкой
+tokenbgd = "vk1.a.f4Cf-9IZro_XjSIoB1XGXpLZf5CIzVkM_RwKosxK7L58nsemL5k60y4X0-4SGzMNRTIZTt8LHalI3NRpDiGuxJI7COlO67JtyQnernUf21yg3mXgUy-4yHIbe1OGNG2KDuqA1Pyga2ys-al4VDQ2_5hbi2IjNzBnA7PKo9RKYwpXo4BpmuAujZi4BnMmygZM0LGw5i-6Gj1Iu2qBkGUruw"
+
+# токен сервисный
+token = "08c5229d08c5229d08c5229de70bd71a77008c508c5229d6b0756ad8e3a68596543fdf7"
